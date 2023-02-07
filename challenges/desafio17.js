b23292c32637f2e8da15373d0763745fa3f36303
MilenaSuzumura <milena.suzumura11@gmail.com>
2023-02-07 15:31:14 -0300

    Desafio 17

---
+++ challenges/desafio17.js
@@ -0,0 +1,6 @@
+db.resumoProdutos.insertMany([{
+  franquia: "McDonalds",
+  totalProdutos: db.produtos.find().count(),
+}]);
+
+db.resumoProdutos.find({}, { franquia: 1, totalProdutos: 1, _id: 0 });
